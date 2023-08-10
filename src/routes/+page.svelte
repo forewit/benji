a360dc0172845555e45f171c51f4771e02c1d609
Anderson <MDAnderson@samaritan.org>
2023-08-10 15:47:14 -0400

    'na'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
 
   let typewriter1: Typewriter;
 
+  let block1 = `Dear Benji (or whoever is reading 😊),
+
+if you are seeing this, please shoot me (Marc) a text!
+
+Talk to ya soon :)`;
+
   async function begin() {
     await typewriter1.start();
     console.log("finished");
@@ -13,7 +19,7 @@
   }
 </script>
 
-<Typewriter bind:this={typewriter1} text="Here is some thrilling content!" />
+<Typewriter bind:this={typewriter1} text={block1} />
 
 <button on:click={begin}>Begin</button>
 <button on:click={reset}>Reset</button>
