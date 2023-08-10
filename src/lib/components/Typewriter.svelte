598398a38ad82e513e6d33b1dd2be750446b7289
Anderson <MDAnderson@samaritan.org>
2023-08-10 13:42:37 -0400

    'na'

--- src/lib/components/Typewriter.svelte
+++ src/lib/components/Typewriter.svelte
@@ -4,62 +4,69 @@
   import { onMount } from "svelte";
 
   export let text: string = "";
-  export let pause: number = 2000;
+  export let startDelay: number = 1000;
   export let minDelay: number = 30;
-  export let maxDelay: number = 300;
+  export let maxDelay: number = 200;
   export let blinkDelay: number = 500;
+  export let disableCursor: boolean = false;
 
-  let isBlinking: boolean = true;
+  let callback: () => any = () => {};
   let displayedText: string = "";
+  let cursor = {
+    text: "| ",
+    isVisible: true,
+    isBlinking: true,
+  };
+
+  export const start = (onComplete?: () => any) => {
+    index = 0;
+    displayedText = "";
+    cursor.isBlinking = true;
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(typeNext, startDelay);
+    if (onComplete) callback = onComplete;
+  };
+
+  function updateCursor() {
+    if (disableCursor) cursor.text = " ";
+    else if (!cursor.isBlinking) cursor.text = "| ";
+    else cursor.text = cursor.text == " " ? "| " : " ";
+    setTimeout(updateCursor, blinkDelay);
+  }
 
   function randBetween(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  let cursor = {
-    text: "|",
-    isVisible: true,
-    isBlinking: true
-  }
-
-  function updateCursor() {
-    if (!cursor.isVisible) cursor.text = "";
-    else if (!cursor.isBlinking) cursor.text = "|";
-    else (cursor.text = cursor.text == "" ? "|" : "");
-    setTimeout(updateCursor, blinkDelay);
-  }
-
+  let timeoutID: number;
   let index = 0;
   function typeNext() {
     if (index >= text.length) {
       cursor.isBlinking = true;
-      cursor.isVisible = true;
+      callback();
       return;
     } else if (index == 0) {
-        cursor.isBlinking = false;
-        cursor.isVisible = true;
-    } 
+      cursor.isBlinking = false;
+    }
 
     let char = text[index];
-    let delay = char == " " ? maxDelay : randBetween(minDelay, maxDelay);
+    let delay = randBetween(minDelay, maxDelay);
+    if (text[index + 1] == " ") delay *= 2;
 
     displayedText += char;
     index++;
-    setTimeout(typeNext, delay);
+    timeoutID = setTimeout(typeNext, delay);
   }
 
   onMount(() => {
     updateCursor();
-    setTimeout(typeNext, pause);
   });
 </script>
 
-<p>{displayedText}{cursor.text}</p>
+<p id="content">{displayedText}{cursor.text}</p>
 
 <style>
-  p::before {
-    width: 10px;
-    height: 10px;
-    background: green;
+  #content {
+    white-space: pre-wrap;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
   import Typewriter from "$lib/components/Typewriter.svelte";
+
+  let typer: Typewriter;
+
+  function getGoing () {
+    typer.start(()=>{
+        console.log("Finished!");
+    })
+  }
 </script>
 
 <h1>Hey Benji</h1>
-<Typewriter text="Here is some thrilling content!"/>
+<Typewriter bind:this={typer} text="Here is some thrilling content!"  />
+<button on:click={getGoing}>Start!</button>
